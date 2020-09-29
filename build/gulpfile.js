5bd020e8c4d907771c69b8c6e23af783a1d144f2
jinfeiyang <jinfeiyang@dxy.cn>
2020-09-29 10:47:03 +0800

    feat: add dev task

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -2,18 +2,24 @@ const gulp = require('gulp');
 const rollup = require('rollup');
 const terser = require("rollup-plugin-terser").terser
 
-gulp.task('build', async function () {
-  const bundle = await rollup.rollup({
-    input: '../lib/index.js',
-    plugins: [
-      terser()
-    ]
-  });
+async function build(){
+    const bundle = await rollup.rollup({
+      input: '../lib/index.js',
+      plugins: [
+        terser()
+      ]
+    });
 
-  await bundle.write({
-    file: '../dist/easyCanvas.min.js',
-    format: 'umd',
-    name: 'easyCanvas',
-    sourcemap: false
-  });
-});
+    await bundle.write({
+      file: '../dist/easy-canvas.min.js',
+      format: 'umd',
+      name: 'easyCanvas',
+      sourcemap:true
+    });
+}
+
+gulp.task('build', build);
+
+gulp.task('default',async function(){
+  gulp.watch('../lib',build)
+})
